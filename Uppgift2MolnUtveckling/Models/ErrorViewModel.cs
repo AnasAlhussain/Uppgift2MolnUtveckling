b843f6b2bc4800da3dc3de11f386dc882d1a3f24
Anas Alhussain <anasalhussain.sweeden@hotmail.com>
2024-11-28 01:12:39 +0100

    update

--- Uppgift2MolnUtveckling/Models/ErrorViewModel.cs
+++ Uppgift2MolnUtveckling/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Uppgift2MolnUtveckling.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
